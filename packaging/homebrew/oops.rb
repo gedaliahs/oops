79923c0853798d3c3a13388985f79cfd58aa98c5
gedaliahs <dylicplayz@gmail.com>
2026-05-28 06:43:45 -0400

    Sync Homebrew formula with v0.5.0 release checksums

--- packaging/homebrew/oops.rb
+++ packaging/homebrew/oops.rb
@@ -7,20 +7,20 @@ class Oops < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/gedaliahs/oops/releases/download/v0.5.0/oops_darwin_arm64.tar.gz"
-      sha256 "bcc6cf2a2827c381b5cea23fca15816da5eb5ed457a72ded0d1b5a172d0faeb1"
+      sha256 "f49101a26c35f1c161711609ad94a0ae4790f76860107ff7677320e180208d24"
     else
       url "https://github.com/gedaliahs/oops/releases/download/v0.5.0/oops_darwin_amd64.tar.gz"
-      sha256 "0954390f8438a43dfe5a5bf09429740bac6e744bad80bfcc2dd0ad5bbab2d35b"
+      sha256 "2fa51dec4abe0d493859b2461a22e35e9b66b71fd55229e6fcf985f9e503e118"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/gedaliahs/oops/releases/download/v0.5.0/oops_linux_arm64.tar.gz"
-      sha256 "b33362bcbf6142ad8ab76162b00609efbfacd8d6e017216dc639e0792fe0c718"
+      sha256 "3feaaf0ab353f46a77e762758c89e85291b5f7a7711fa607ee3a0ae7ddc94e67"
     else
       url "https://github.com/gedaliahs/oops/releases/download/v0.5.0/oops_linux_amd64.tar.gz"
-      sha256 "b0e2a246a63966ef70e1c1bb3c44bddde5552fa28ec2a16222a2484fa21a1e7b"
+      sha256 "04985df74949175dca34c98afd1e874fe7cd25e6b63ea11ca38e10aedcaeea82"
     end
   end
 
